9f2c47a605d0f967bbdebb98f8a5ae3a475815ec
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-22 15:38:45 +0100

    Created array of object

--- js/main.js
+++ js/main.js
@@ -3,3 +3,59 @@
 // elettrica, ibrida e gpl).
 // dividi le automobili in 3 array separati, 1 per quelle a benzina, 1 per 
 // diesel e 1 per le altre, in fine stampa i 3 array separatamente.
+
+// inizio esercizio, creo un array di oggetti con 10 auto
+
+let carList = [
+    {
+        brand: "Alfa Romeo",
+        model: "Tonale",
+        fuel: "Ibrida"
+    },
+    {
+        brand: "Audi",
+        model: "RS 5 SPB",
+        fuel: "Benzina"
+    },
+    {
+        brand: "BMW",
+        model: "Serie 1",
+        fuel: "Diesel"
+    },
+    {
+        brand: "Mercedes-Benz",
+        model: "Classe A 5p",
+        fuel: "Diesel"
+    },
+    {
+        brand: "Ferrari",
+        model: "Stradale",
+        fuel: "Benzina"
+    },
+    {
+        brand: "Ford",
+        model: "Fiesta",
+        fuel: "GPL"
+    },
+    {
+        brand: "Lamborghini",
+        model: "Aventador",
+        fuel: "Benzina"
+    },
+    {
+        brand: "Maserati",
+        model: "Ghibli",
+        fuel: "Diesel"
+    },
+    {
+        brand: "Mazda",
+        model: "Mazda 3",
+        fuel: "Idrida"
+    },
+    {
+        brand: "Tesla",
+        model: "Model s",
+        fuel: "Elettrica"
+    }
+];
+
