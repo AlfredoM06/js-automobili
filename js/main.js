699dff1c18a21c429ce1a96506fe9de4253bcd40
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-22 15:00:12 +0100

    started repo

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Esercizio, creare un array composto da 10 automobili. Ogni oggetto avra
+// come proprietà le seguenti: Marca, Modello e Alimentazione(benzian,diesel
+// elettrica, ibrida e gpl).
+// dividi le automobili in 3 array separati, 1 per quelle a benzina, 1 per 
+// diesel e 1 per le altre, in fine stampa i 3 array separatamente.
